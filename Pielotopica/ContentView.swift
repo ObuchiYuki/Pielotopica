2e5b0bb383794fa5f02c4850d0db22566bc0cb2b
yuki <yukibochi1@gmail.com>
2019-08-11 23:31:10 +0900

    Initial Commit

---
+++ Pielotopica/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pielotopica
+//
+//  Created by yuki on 2019/08/11.
+//  Copyright © 2019 yuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
