18c25f71e8e917f92a32917d882828f8bebbca39
yuki <yukibochi1@gmail.com>
2019-08-26 09:56:12 +0900

    TSFuelData

---
+++ Pielotopica/SandBox/SandBox/Manager/TSFuelData.swift
@@ -0,0 +1,23 @@
+//
+//  TSFuelData.swift
+//  Pielotopica
+//
+//  Created by yuki on 2019/08/26.
+//  Copyright © 2019 yuki. All rights reserved.
+//
+
+import RxCocoa
+
+struct TSFuelData: RMAutoSavable {
+    static var shared:TSFuelData {get {autosave.value} set {autosave.value = newValue}}
+    
+    var heart = BehaviorRelay(value: 0)
+    var fuel = BehaviorRelay(value: 0)
+    
+    mutating func setHeart(_ amount:Int) {
+        heart.accept(amount)
+    }
+    mutating func setFuel(_ amount:Int) {
+        fuel.accept(amount)
+    }
+}
