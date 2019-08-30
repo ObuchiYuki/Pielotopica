5d378532809a424978d3f9c7be6ffdb660aef856
yuki <yukibochi1@gmail.com>
2019-08-30 12:54:42 +0900

    kari target

--- Pielotopica/SandBox/Impl/Blocks/TS_TargetBlock.swift
+++ Pielotopica/SandBox/Impl/Blocks/TS_TargetBlock.swift
@@ -11,7 +11,7 @@ import Foundation
 class TS_TargetBlock: TSBlock {
     
     init(index:Int) {
-        super.init(nodeNamed: "TS_target_kari", index: index)
+        super.init(nodeNamed: "TP_target_kari", index: index)
     }
     
     override func getOriginalNodeSize() -> TSVector3 {
