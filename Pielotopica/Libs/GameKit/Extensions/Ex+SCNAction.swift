ab078eb1c9ac3fe0020e34899bbe1d2cce47abd7
yuki <yukibochi1@gmail.com>
2019-09-02 16:32:55 +0900

    action

--- Pielotopica/Libs/GameKit/Extensions/Ex+SCNAction.swift
+++ Pielotopica/Libs/GameKit/Extensions/Ex+SCNAction.swift
@@ -9,6 +9,15 @@
 import SceneKit
 
 public extension SCNAction {
+    class func cameraZoom(to scale:Double, duration seconds: TimeInterval) -> SCNAction {
+        return SCNAction.customAction(duration: seconds) { node, time in
+            guard let camera = node.camera else {return}
+            let ratio = Double(time) / seconds
+            
+            camera.orthographicScale = scale * ratio
+        }
+    }
+
     func setEase(_ ease:SCNActionTimingMode) -> SCNAction{
         self.timingMode = ease
         return self
