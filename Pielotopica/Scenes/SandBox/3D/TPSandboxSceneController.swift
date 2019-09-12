1c294be0cae29a8fa396b83ec7da873a58031247
yuki <yukibochi1@gmail.com>
2019-09-12 10:57:33 +0900

    directionalLight

--- Pielotopica/Scenes/SandBox/3D/TPSandboxSceneController.swift
+++ Pielotopica/Scenes/SandBox/3D/TPSandboxSceneController.swift
@@ -183,11 +183,11 @@ extension TPSandboxSceneController {
     func setupDirectionalLight() {
         directionalLightNode.eulerAngles = [-.pi/4, -.pi/4, 0]
         
-        //directionalLight.castsShadow = true
+        directionalLight.castsShadow = true
         //directionalLight.shadowMapSize = [1000, 1000]
         //directionalLight.maximumShadowDistance = 2000
-        //directionalLight.shadowColor = UIColor.black.withAlphaComponent(0.8)
-        //directionalLight.shadowSampleCount = 3
+        directionalLight.shadowColor = UIColor.black.withAlphaComponent(0.8)
+        directionalLight.shadowSampleCount = 3
         
     }
     func setupCamera() {
