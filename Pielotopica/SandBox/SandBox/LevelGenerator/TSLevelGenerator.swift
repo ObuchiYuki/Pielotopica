3fe37e9ae025a5bc8add66dc85ff0daf69a88604
yuki <yukibochi1@gmail.com>
2019-09-13 10:49:13 +0900

    ÷TSLevelGenerator

---
+++ Pielotopica/SandBox/SandBox/LevelGenerator/TSLevelGenerator.swift
@@ -0,0 +1,16 @@
+//
+//  TSLevelGenerator.swift
+//  Pielotopica
+//
+//  Created by yuki on 2019/09/13.
+//  Copyright © 2019 yuki. All rights reserved.
+//
+
+import Foundation
+
+
+class TSLevelGenerator {
+    static let shared = TSLevelGenerator()
+
+    func level
+}
