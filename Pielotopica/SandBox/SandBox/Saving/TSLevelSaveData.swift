7e2a7cde6c73b1d8d3f91fa07b7adaa47a40fc33
yuki <yukibochi1@gmail.com>
2019-09-13 17:59:57 +0900

    TSLevelSaveData

--- Pielotopica/SandBox/SandBox/Saving/TSLevelSaveData.swift
+++ Pielotopica/SandBox/SandBox/Saving/TSLevelSaveData.swift
@@ -8,101 +8,27 @@
 
 import Foundation
 
-public class TSLevelSaveData {
+private class TSLevelSaveData: Codable {
     
+    // MARK: - Singleton -
+    public static let shared = TSLevelSaveData()
     
-    
-    /// whether the level is properly initialized.
-    public var initialized:Bool = false
-    
-    ///  The name of the level. defalut is "default".
-    public var levelName: String
-    
-    /// The name of the generator.
-    public var generatorName:GeneratorName
-    public enum GeneratorName: UInt8, Codable {case `default` = 0, flat = 1, debug = 2}
-    
-    /// The random level seed used to generate consistent terrain.
-    public var randomSeed: String
-    
-    ///  The game rules.
-    public var gameRules: GameRules
-    
-    public struct GameRules: Codable {
-        /// whether the debug mode on.
-        public var debug: Bool = false
-            
-    }
-    
-    /// プレイヤーデータ
-    public var player:Player
-    
-    public struct Player: Codable {
-        
-        /// 現在のプレイヤー位置
-        public var positionX: Int16 = 0
-        public var positionY: Int16 = 0
-        public var positionZ: Int16 = 0
-        
-        /// プレイヤーが存在するディメンション。今のところ 0 (= 地上) のみ
-        public var dimension: UInt8 = 0
-        
-        /// 現在のスコア
-        public var score: Int32 = 0
-        
-        /// プレイヤーの選択したホットバーのスロット。
-        public var selectedItemSlot: UInt8 = 0
-        
-        /// 現在のアイテムバーのアイテム
-        public var selectedItems: [Item] = []
-        
-        public struct Item: Codable {
-            ///  アイテムのスタック数。
-            public var count: UInt32 = 0
-            
-            /// アイテムのあるスロット番号。
-            public var slot: UInt32 = 0
-            
-            /// アイテムの `id`
-            public var id: UInt16 = 0
-            
-            /// アイテムのデータ値。道具の場合は「ダメージ値」になる。
-            public var data:UInt16 = 0
-        }
-    }
-    
-    /// Information about the Topica version the world was saved in.
-    public var version: Version = .current
-    
-    public struct Version: Codable {
-        /// An identifier for the version.
-        public var id: Int32
-        /// The version name as a string, e.g. "tp.1.0.1"
-        public var name: String
-        /// Whether the version is debug
-        public var debug: Bool
-    }
+    // MARK: - Constructor -
     
     init(levelName: String, generatorName: GeneratorName, randomSeed: String) {
         self.levelName = levelName
         self.generatorName = generatorName
         self.randomSeed = randomSeed
     }
-}
-
-extension RMStorage.Key {
-    fileprivate static var _TSLevelSaveDataKey:RMStorage.Key<_TSLevelSaveData> { return RMStorage.Key(rawValue: "level.box") }
-}
-
-private class TSLevelSaveData: Codable {
     
-    public static let shared = TSLevelSaveData()
+    
+    // MARK: - Properties -
     
     /// version of save data.
-    var saveVersion: Int16
+    var saveVersion: Int16 = 0
     
     /// whether the level is properly initialized.
-    var initialized:Bool
+    var initialized:Bool = 0
     
     ///  The name of the level. defalut is "default".
     var levelName: String
@@ -171,10 +97,8 @@ private class TSLevelSaveData: Codable {
         /// Whether the version is debug
         var debug: Bool
     }
-    
-    
-    
-    init(data: TSLevelSaveData) {
-        //...
-    }
+}
+
+extension RMStorage.Key {
+    fileprivate static var _TSLevelSaveDataKey:RMStorage.Key<_TSLevelSaveData> { return RMStorage.Key(rawValue: "level.box") }
 }
