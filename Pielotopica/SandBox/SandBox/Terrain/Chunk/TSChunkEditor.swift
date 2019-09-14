34f068b819a4546675eed1b5c2b21262d8f6fa5c
yuki <yukibochi1@gmail.com>
2019-09-14 18:58:06 +0900

    TSChunkEditor

---
+++ Pielotopica/SandBox/SandBox/Terrain/Chunk/TSChunkEditor.swift
@@ -0,0 +1,13 @@
+//
+//  TSChunkEditor.swift
+//  Pielotopica
+//
+//  Created by yuki on 2019/09/14.
+//  Copyright © 2019 yuki. All rights reserved.
+//
+
+import Foundation
+
+public class TSChunkEditor {
+    public static let shared = TSChunkEditor()
+}
