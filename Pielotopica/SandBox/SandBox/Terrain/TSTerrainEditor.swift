82c8e3d56fb659a2fc6e03a644d9496057dc6684
yuki <yukibochi1@gmail.com>
2019-09-14 19:27:23 +0900

    delegates

--- Pielotopica/SandBox/SandBox/Terrain/TSTerrainEditor.swift
+++ Pielotopica/SandBox/SandBox/Terrain/TSTerrainEditor.swift
@@ -8,17 +8,51 @@
 
 import Foundation
 
+protocol TSTerrainEditorDelegate {
+    func editor(levelDidUpdateBlockAt position:TSVector3, needsAnimation animiationFlag:Bool, withRotation rotation:TSBlockRotation)
+    func editor(levelWillDestoryBlockAt position:TSVector3)
+    func editor(levelDidDestoryBlockAt position:TSVector3)
+}
+
 // MARK: - TSTerrainEditor -
 
 public class TSTerrainEditor {
     // MARK: - Singleton -
     public static let shared = TSTerrainEditor()
     
+    public var delegates = RMWeakSet<TSTerrainEditorDelegate>()
+    
     // MARK: - Methods -
     
     @discardableResult
     public func placeBlock(_ block:TSBlock, at anchor:TSVector3, rotation:TSBlockRotation, forced:Bool = false) -> Bool {
-        return true
+        var rotation = rotation
+        var anchor = anchor
+        
+        if block.shouldRandomRotateWhenPlaced() {
+            
+            rotation = TSBlockRotation.random
+            
+            anchorPoint = TSModelRotator.shared.calcurateAnchorPoint(
+                blockSize: block.getSize(at: anchorPoint),
+                initial: anchorPoint,
+                for: rotation
+            )
+        }
+        
+        self._writeRotation(rotation, at: anchorPoint)
+        
+        block.willPlace(at: anchorPoint)
+        
+        self.anchorMap.insert(anchorPoint)
+        self._setAnchoBlockMap(block, at: anchorPoint)
+        self._fillFillMap(with: block, at: anchorPoint, blockSize: block.getSize(at: anchorPoint))
+        
+        delegates.forEach{$0.level(self, levelDidUpdateBlockAt: anchorPoint, needsAnimation: true, withRotation: rotation)}
+        
+        block.didPlaced(at: anchorPoint)
+        
+        //self._save()
     }
     
     @discardableResult
