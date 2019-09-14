2f90025e48521493a85f20131047a73b99ba8cb9
yuki <yukibochi1@gmail.com>
2019-09-14 10:30:48 +0900

    var musicVolume: Float = 0.5

--- Pielotopica/SandBox/SandBox/Saving/TSOptionSaveData.swift
+++ Pielotopica/SandBox/SandBox/Saving/TSOptionSaveData.swift
@@ -11,5 +11,15 @@ import Foundation
 class TSOptionSaveData {
     static let shared = TSOptionSaveData()
     
+    /// 設定ファイルのバージョン番号。
+    var version: String
     
+    /// プレイヤーから見えるチャンクの描画距離半径
+    var renderDistance: Int = 10
+    
+    /// 音楽の音量
+    var musicVolume: Float = 0.5
+    
+    /// 効果音の音量　
+    var seVolume: Float = 0.5
 }
