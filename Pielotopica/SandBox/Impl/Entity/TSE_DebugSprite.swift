8306e0f26f88d7f5d29f2b72c1392b68e805e95b
yuki <yukibochi1@gmail.com>
2019-09-17 21:43:50 +0900

    coder

--- Pielotopica/SandBox/Impl/Entity/TSE_DebugSprite.swift
+++ Pielotopica/SandBox/Impl/Entity/TSE_DebugSprite.swift
@@ -11,3 +11,16 @@ import SpriteKit
 class TSE_DebugSprite: TSSprite {
     
 }
+
+private class DebugSprite: SKSpriteNode {
+    let label = SKLabelNode()
+    
+    init() {
+        super.init(texture: nil, color: .purple, size: [100, 30])
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
