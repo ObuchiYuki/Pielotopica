1e1dc62b0f0d97b2e34d89094ce3fba0a18179fc
yuki <yukibochi1@gmail.com>
2019-09-17 21:40:14 +0900

    TSE_DebugSprite

---
+++ Pielotopica/SandBox/Impl/Entity/TSE_DebugSprite.swift
@@ -0,0 +1,13 @@
+//
+//  TSE_DebugSprite.swift
+//  Pielotopica
+//
+//  Created by yuki on 2019/09/17.
+//  Copyright © 2019 yuki. All rights reserved.
+//
+
+import SpriteKit
+
+class TSE_DebugSprite: TSSprite {
+    
+}
