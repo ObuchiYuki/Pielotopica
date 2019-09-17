127308b8bc4b9a45b47ac714c08e01334270b914
yuki <yukibochi1@gmail.com>
2019-09-17 22:00:09 +0900

    enableDebug()

--- Pielotopica/SandBox/SandBox/Terrain/TSTerrainManager.swift
+++ Pielotopica/SandBox/SandBox/Terrain/TSTerrainManager.swift
@@ -161,8 +161,8 @@ public class TSTerrainManager {
     #if DEBUG
     public func enableDebug() {
         for x in -10...10 {
-            for x in -10...10 {
-                
+            for z in -10...10 {
+                TSE_DebugSprite.show(at: TSVector3(x * 16, 2, z * 16), with: "\(x), \(z)")
             }
         }
     }
